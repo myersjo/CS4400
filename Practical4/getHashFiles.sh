219888c3c154cf67785257b6678b61dc000d1a1c
root <root@localhost.localdomain>
2018-10-08 20:00:52 +0000

    Added separate hash files

--- Practical4/getHashFiles.sh
+++ Practical4/getHashFiles.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-FSHA256="sha256.hashes"
+FHashes="myersjo.hashes"
 
-sudo gawk '/\$pbkdf2-sha256\$/ { print }' myersjo.hashes >> sha256.hashes
+FSHA256="sha256.hashes"
+FMD5Crypt="md5crypt.hashes"
+FSHA256Crypt="sha256crypt.hashes"
+FSHA512Crypt="sha512crypt.hashes"
+FARGON2i="argon2i.hashes"
+FOther="other.hashes"
+
+sudo gawk '/\$pbkdf2-sha256\$/ { print }' $FHashes >> $FSHA256
+sudo gawk '/\$1\$/ { print }' $FHashes >> $FMD5Crypt
+sudo gawk '/\$5\$/' $FHashes >> $FSHA256Crypt
+sudo gawk '/\$6\$/' $FHashes >> $FSHA512Crypt
+sudo gawk '/\$argon2i\$/' $FHashes >> $FARGON2i
+sudo gawk '$0 !~ /\$/' $FHashes >> $FOther
