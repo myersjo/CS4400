80f32380a50219bee3698c7fef1083d4248619b6
root <root@ip-172-30-2-140.eu-west-1.compute.internal>
2018-10-14 16:10:47 +0000

    Descrypt

--- Practical4/descryptfix.sh
+++ Practical4/descryptfix.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-BROKEN_IN="descrypt.broken"
-BROKEN_OUT="descryptfix.broken"
+BROKEN_IN="descrypt-cs-tmp.broken"
+BROKEN_OUT="descrypt-csfix.broken"
 
 sudo gawk '{ print $1, substr($2,1,7) }' $BROKEN_IN > $BROKEN_OUT
 
